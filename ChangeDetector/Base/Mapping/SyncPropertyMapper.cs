511d8e13d1d1cf5a638856e68740d8fb6619362a
Dmitry <d.chernenko@trace-x.ru>
2026-01-19 00:19:04 +0300

    Фикс сериализатора

---
+++ ChangeDetector/Base/Mapping/SyncPropertyMapper.cs
@@ -0,0 +1,34 @@
+﻿namespace Assets.Shared.ChangeDetector.Base.Mapping
+{
+    // DTO для SyncProperty значения
+    public class SyncPropertyDto
+    {
+        public object? Value { get; set; }
+        public string TypeName { get; set; }
+    }
+
+    // Маппер для SyncProperty
+    [SyncDto(typeof(SyncProperty<>))]
+    public sealed class SyncPropertyMapper : ISyncValueMapper
+    {
+        public object ToDto(object modelValue)
+        {
+            var propType = modelValue.GetType();
+            var valueProperty = propType.GetProperty("Value");
+            var value = valueProperty?.GetValue(modelValue);
+
+            return new SyncPropertyDto
+            {
+                Value = SyncValueConverter.ToDtoIfNeeded(value),
+                TypeName = propType.GenericTypeArguments[0].AssemblyQualifiedName
+            };
+        }
+
+        public object FromDto(object dtoValue)
+        {
+            var dto = (SyncPropertyDto)dtoValue;
+            // SyncProperty воссоздается автоматически SyncNode
+            return SyncValueConverter.FromDtoIfNeeded(dto.Value);
+        }
+    }
+}
